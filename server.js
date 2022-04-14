23b34c78240bee7bdabe023a2ac166615a467dfa
Matt Habbo <matthabbo45@gmail.com>
2022-04-14 11:13:44 -0400

    MVP

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const req = require('express/lib/request');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const { animals } = require('./data/animals');
+const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -40,12 +41,27 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+    
 });
 
 // listens for a connection on the given path
